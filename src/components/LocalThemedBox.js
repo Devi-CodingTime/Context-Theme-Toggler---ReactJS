cf03297bfc9a9baeb09c784678dea07b7bf56f1b
Devi-CodingTime <devipurnima26899@gmail.com>
2023-12-15 21:18:23 +0530

    last changes

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -5,10 +5,10 @@ const {theme} = props;
 return(
     <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={`bg-${theme==="light"?"dark":"light"}`}>
         {/* Write code below this line */}
-        <p id="local-themed-text-container" className={`txt-${theme}`}>
+        <p id="local-themed-text-container" className={`txt-${theme==="light"?"dark":"light"}`}>
         lorem ipsum dolor iterit n stuff
         </p>
-        <button id="local-theme-toggler" className={`btn btn-${theme} txt-${theme}`}>{`Toggle local theme to ${theme}`}</button>
+        <button id="local-theme-toggler" className={`btn btn-${theme==="light"?"dark":"light"} txt-${theme==="light"?"dark":"light"}`}>{`Toggle local theme to ${theme==="light"?"dark":"light"}`}</button>
     </div>
 )
 }
