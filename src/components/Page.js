0a79a34575aba96a429b8ecf2c3efe824e3e8ca5
Devi-CodingTime <devipurnima26899@gmail.com>
2023-12-15 21:09:40 +0530

    last changes

--- src/components/Page.js
+++ src/components/Page.js
@@ -7,7 +7,7 @@ const Page = () => {
     const{theme} = useContext(ThemeContext);
     return(
         <div className={`container bg-${theme}`} id="themed-page">
-            <p id={`themed-text-container txt-${theme}`}>
+            <p id='themed-text-container' className={`txt-${theme}`}>
                 lorem ipsum dolor iterit n stuff
             </p>
             <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">Themed Button</button>
