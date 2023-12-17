6c379a38b7929f1f7ba504c5b6f3a631486017b1
Devi-CodingTime <devipurnima26899@gmail.com>
2023-12-17 11:46:13 +0530

    theme change

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LocalThemedBox = (props) => {
-const {theme} = props;
+    const [loaclTheme, setLlocalTheme] = useState(props.theme);
+    const {theme} = props;
+
+    useEffect(()=>{
+        setLlocalTheme(theme);
+    },[theme]);
 return(
-    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={`bg-${theme==="light"?"dark":"light"}`}>
+    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={`bg-${loaclTheme}`}>
         {/* Write code below this line */}
-        <p id="local-themed-text-container" className={`txt-${theme==="light"?"dark":"light"}`}>
+        <p id="local-themed-text-container" className={`txt-${loaclTheme}`}>
         lorem ipsum dolor iterit n stuff
         </p>
-        <button id="local-theme-toggler" className={`btn btn-${theme==="light"?"dark":"light"} txt-${theme==="light"?"dark":"light"}`}>{`Toggle local theme to ${theme==="light"?"dark":"light"}`}</button>
+        <button id="local-theme-toggler" onClick={()=>{setLlocalTheme(loaclTheme==="light"?"dark":"light")}} className={`btn btn-${loaclTheme} txt-${loaclTheme}`}>{`Toggle local theme to ${loaclTheme==="light"?"dark":"light"}`}</button>
     </div>
 )
 }
